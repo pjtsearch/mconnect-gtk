4e5051b801cc7f469e578ab1eedc3dca4edeed80
PJTSearch <pjtsignups@gmail.com>
2020-12-21 18:30:11 -0800

    feat(build.rs): Add scss compiling

---
+++ build.rs
@@ -0,0 +1,12 @@
+use std::fs::OpenOptions;
+use sass_rs::*;
+use std::io::Write;
+
+fn main() {
+    let mut file = OpenOptions::new()
+                .write(true)
+                .create(true)
+                .open("src/views/App.css")
+                .unwrap();
+    file.write(compile_file("src/views/App.scss", Options::default()).unwrap().as_bytes()).unwrap();
+}
